57c3d2cfbfc93930266e26daae4793055a5cea8e
robinkii <robinkelsickii@gmail.com>
2025-03-01 13:56:30 -0500

    remove environment configuration file

--- angular-rebuild/src/environments/environments.ts
+++ angular-rebuild/src/environments/environments.ts
@@ -1,4 +1,4 @@
-export const environment ={
-    production: false,
-    openWeatherApiKey: '502fcb49e24ae8c0fb36c4f908f48cda',
-}
+export const environment = {
+  production: false,
+  openWeatherApiKey: '502fcb49e24ae8c0fb36c4f908f48cda',
+};
