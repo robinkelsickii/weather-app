a9ef91eb14df69572b6ced1838cdb94a3565178b
robinkii <robinkelsickii@gmail.com>
2025-03-01 14:47:53 -0500

    base vue set up

---
+++ vue-rebuild/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Vue 3 + Vite + TypeScript</h1>
+    <p>Vue 3 + Vite + TypeScript</p>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ vue-rebuild/src/models/Weather.ts
@@ -0,0 +1,8 @@
+export interface Weather {
+  city: string;
+  temperature: number;
+  description: string;
+  icon: string;
+  windSpeed: number;
+  humidity: number;
+}
