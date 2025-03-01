bf306e7b8fe0cc6de57bcef2f67ccba440421907
Robin Kelsick II <robinkelsickii@gmail.com>
2025-03-01 04:51:36 +0000

    add current weather, forecast, and settings components with initial templates and tests

---
+++ angular-rebuild/src/app/features/current-weather/current-weather.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-current-weather',
+  imports: [],
+  templateUrl: './current-weather.component.html',
+  styleUrl: './current-weather.component.css'
+})
+export class CurrentWeatherComponent {
+
+}
---
+++ angular-rebuild/src/app/features/forcast/forcast.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forcast',
+  imports: [],
+  templateUrl: './forcast.component.html',
+  styleUrl: './forcast.component.css'
+})
+export class ForcastComponent {
+
+}
