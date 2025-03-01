38deee0ebac1c758d3cdadd275d1a6225b0966bc
Robin Kelsick II <robinkelsickii@gmail.com>
2025-03-01 05:08:20 +0000

    add weather service with base URL and API key; create environment configuration for OpenWeather API

--- angular-rebuild/src/app/core/services/weather.service.ts
+++ angular-rebuild/src/app/core/services/weather.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WeatherService {
+  private baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+  private apiKey
 
-  constructor() { }
+  constructor() {}
 }
